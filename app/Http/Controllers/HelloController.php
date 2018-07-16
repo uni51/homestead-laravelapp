570fd07ed7c2fb0ce30f7a44857fde9811f2c738
vagrant <vagrant@homestead>
2018-07-16 02:14:05 +0000

    finish chapter3

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,27 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-  public function index() {
+   public function index() {
+     return view('hello.index', ['message'=>'HogeHoge!']);
+   }
 
-      return <<<EOF
-<html>
-<head>
-<title>Hello/Index</title>
-<style>
-body {font-size:16pt; color:#999; }
-h1 { font-size:100pt; text-align:right; color:#eee;
-  margin:-40px 0px -50px 0px; }
-</style>
-</head>
-<body>
-  <h1>Index</h1>
-  <p>これは、Helloコントローラのindexアクションです。</p>
-</body>
-</html>
-EOF;
-
-  }
 }
