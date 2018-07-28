9fe9460947500333b20077c39c182f91ce7d1ba0
uni51 <uni51@users.noreply.github.com>
2018-07-28 17:02:35 +0900

    chapetr6-3 finished

--- app/Person.php
+++ app/Person.php
@@ -4,17 +4,25 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use App\Scopes\ScopePerson;
+// use App\Scopes\ScopePerson;
 
 
 class Person extends Model
 {
     // グローバルスコープ
-    protected static function boot()
-    {
-       parent::boot();
-       static::addGlobalScope(new ScopePerson);
-    }
+    // protected static function boot()
+    // {
+    //    parent::boot();
+    //    static::addGlobalScope(new ScopePerson);
+    // }
+
+    protected $guarded = array('id');
+
+    public static $rules = array(
+       'name' => 'required',
+       'mail' => 'email',
+       'age' => 'integer|min:0|max:150'
+    );
 
     public function getData()
     {
