1ecb8bdd2161f969c271c68057f5636e2869d203
uni51 <uni51@users.noreply.github.com>
2018-07-29 19:33:30 +0900

    cahpter7-3 pagination finished

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,14 +6,18 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
 use Validator;
- use Illuminate\Support\Facades\DB;
+use App\Person;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::table('people')->get();
-        return view('hello.index', ['items' => $items]);
+        $sort = $request->sort;
+        // $items = DB::table('people')->simplePaginate(5);
+        $items = Person::orderBy($sort, 'asc')->simplePaginate(5);
+        $param = ['items' => $items, 'sort' => $sort];
+        return view('hello.index', $param);
     }
 
     public function post(Request $request)
